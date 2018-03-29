1d729e028ca9b5aff73c6c3abeb5a7a6b2abff0a
Glenn Galvizo <glennjgalvizo@gmail.com>
2018-03-28 21:32:45 -1000

    Current status of the two main runners.

--- parDBd.py
+++ parDBd.py
@@ -11,6 +11,7 @@ OP : 'YS' -> Execute an insertion SQL statement, and wait for additional stateme
    : 'K' -> Record partitioning information to the catalog database.
    : 'U' -> Lookup the node URIs on the catalog database and return these.
    : 'P' -> Lookup the fields for a given table and return this.
+   : 'B' -> Ship a given table to the current node, and perform a join.
 
 Usage: python parDBd.py [hostname] [port]
 
@@ -19,12 +20,15 @@ Error: 2 - Incorrect number of arguments.
 """
 
 import pickle
+import random
 import socket
-import sqlite3 as sql
+import string
 import sys
+import time
 
-from catalog import LocalCatalog
-from dissect import SQLFile
+from lib.catalog import LocalCatalog
+from lib.dissect import SQLFile, ClusterCFG
+from lib.error import ErrorHandle
 
 
 def insert_single_on_db(k_n, r):
@@ -35,23 +39,22 @@ def insert_single_on_db(k_n, r):
     :param r: List passed to socket, containing the name of the database file and the SQL.
     :return: None.
     """
+    # Handle all errors by sending the error string through the socket.
     f, s, tup, r_i = r[1], r[2], r[3], r
-    try:
-        conn = sql.connect(f)
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
+    handle_error = lambda a: ErrorHandle.write_socket(k_n, str(a))
+
+    # Create our connection.
+    c = ErrorHandle.sql_connect(f, handle_error)
+    if ErrorHandle.is_error(c):
         return
-    cur = conn.cursor()
+    conn, cur = c
 
-    # Execute the command. Return the error if any exist.
-    try:
-        cur.execute(s, tup)
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
-
-    # Otherwise, no error exists. Send the success message.
-    conn.commit(), conn.close()
-    k_n.send(pickle.dumps(['EY', 'Success']))
+    # Execute the command. Return the error if any exist, or return a sucesss method.
+    if not ErrorHandle.is_error(ErrorHandle.sql_execute(cur, s, handle_error, tup)):
+        conn.commit(), conn.close()
+        ErrorHandle.write_socket(k_n, ['EY', 'Success'])
+    else:
+        conn.close()
 
 
 def insert_on_db(k_n, r):
@@ -62,38 +65,39 @@ def insert_on_db(k_n, r):
     :param r: List passed to socket, containing the name of the database file and the SQL.
     :return: None.
     """
-    f, s, tup, r_i = r[1], r[2], r[3], r
-    try:
-        conn = sql.connect(f)
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
-        return
-    cur = conn.cursor()
+    # Handle all errors by sending the error string through the socket.
+    f, s, tup, r_i, is_error = r[1], r[2], r[3], r, False
+    handle_error = lambda a: ErrorHandle.write_socket(k_n, str(a))
 
-    # Wait for the terminating operation codes.
+    # Create our connection.
+    c = ErrorHandle.sql_connect(f, handle_error)
+    if ErrorHandle.is_error(c):
+        return
+    conn, cur = c
+
     while True:
         # Execute the command. Return the error if any exist.
-        try:
-            cur.execute(s, tup)
-            k_n.send(pickle.dumps(['EY', 'Success']))
-            r_i = pickle.loads(k_n.recv(4096))
+        if not ErrorHandle.is_error(ErrorHandle.sql_execute(cur, s, handle_error, tup)):
+            ErrorHandle.write_socket(k_n, ['EY', 'Success'])
 
-            # Do not proceed if 'YY' (stop waiting, don't execute) operation code is passed.
-            if r_i[0] == 'YY':
-                break
-            elif r_i[0] == 'YZ':
-                cur.execute(r_i[2], r_i[3])
-                k_n.send(pickle.dumps(['EY', 'Success']))
-                break
-            else:
-                f, s, tup = r_i[1], r_i[2], r_i[3]
+        # Interpret the current operation code.
+        r_i = ErrorHandle.read_socket(k_n, handle_error)
+        if ErrorHandle.is_error(r_i):
+            continue
 
-        except (sql.Error, EOFError) as e_1:
-            k_n.send(pickle.dumps(str(e_1)))
+        # Stop if YZ or YY, proceed otherwise.
+        if r_i[0] == 'YY':
+            break
+        elif r_i[0] == 'YZ' and not \
+                ErrorHandle.is_error(ErrorHandle.sql_execute(cur, s, handle_error, tup)):
+            ErrorHandle.write_socket(k_n, ['EY', 'Success'])
+            break
+        else:
+            f, s, tup = r_i[1], r_i[2], r_i[3]
 
-    # Otherwise, no error exists. Commit our changes and close our connection.
+    # Commit our changes and close our connection.
     conn.commit(), conn.close()
-    k_n.send(pickle.dumps(['EY', 'Success']))
+    ErrorHandle.write_socket(k_n, ['EY', 'Success'])
 
 
 def execute_on_db(k_n, r):
@@ -104,36 +108,39 @@ def execute_on_db(k_n, r):
     :param r: List passed to socket, containing the name of the database file and the SQL.
     :return: None.
     """
+    # Handle all errors by sending the error string through the socket.
     f, s, result = r[1], r[2], []
-    try:
-        conn = sql.connect(f)
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
+    handle_error = lambda a: ErrorHandle.write_socket(k_n, str(a))
+
+    # Create our connection.
+    c = ErrorHandle.sql_connect(f, handle_error)
+    if ErrorHandle.is_error(c):
         return
-    cur = conn.cursor()
+    conn, cur = c
+
+    # Message length must match. Return an error if this is false.
+    if len(r) != 3:
+        handle_error('Error: Incorrect number of items sent to socket.')
+        return
 
     # Execute the command. Return the error if any exist.
-    try:
-        if len(r) == 3:
-            result = cur.execute(s).fetchall()
-        else:
-            k_n.send(pickle.dumps('Incorrect number of items sent to socket.'))
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
-
-    # Otherwise, no error exists. Send the resulting tuples (if any exist).
-    conn.commit(), conn.close()
+    result = ErrorHandle.sql_execute(cur, s, handle_error, fetch=True)
+    if ErrorHandle.is_error(result):
+        conn.close()
+        return
+    else:
+        conn.commit(), conn.close()
 
     # If the statement is a selection, send all resulting tuples.
     if SQLFile.is_select(s):
         for i, r_t in enumerate(result):
             # If this is the last tuple, append the ending operation code.
-            k_n.send(pickle.dumps(['EZ' if i + 1 == len(result) else 'ES', r_t]))
+            ErrorHandle.write_socket(k_n, ['EZ' if i + 1 == len(result) else 'ES', r_t])
         if len(result) == 0:
-            k_n.send(pickle.dumps(['EZ', 'No tuples found.']))
+            ErrorHandle.write_socket(k_n, ['EZ', 'No tuples found.'])
     else:
         # Otherwise, assume that the statement has passed.
-        k_n.send(pickle.dumps(['EZ', 'Success']))
+        ErrorHandle.write_socket(k_n, ['EZ', 'Success'])
 
 
 def return_columns(k_n, r):
@@ -144,24 +151,141 @@ def return_columns(k_n, r):
     :return: None.
     """
     f, tname, col = r[1], r[2], []
-    try:
-        conn = sql.connect(f)
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
+    c = ErrorHandle.sql_connect(f, lambda a: ErrorHandle.write_socket(k_n, str(a)))
+    if ErrorHandle.is_error(c):
         return
-    cur = conn.cursor()
+    conn, cur = c
 
     # Execute the command. Return the error if any exist.
-    try:
-        col = cur.execute('SELECT * FROM ' + tname + ' LIMIT 1').description
-    except sql.Error as e_1:
-        k_n.send(pickle.dumps(str(e_1)))
-
-    # Otherwise, no error exists. Send the resulting tuples (if any exist).
-    conn.commit(), conn.close()
+    col = ErrorHandle.sql_execute(cur, 'SELECT * '
+                                       'FROM ' + tname + 'LIMIT 1;',
+                                  lambda a: ErrorHandle.write_socket(k_n, str(a)),
+                                  fetch=True, desc=True)
+    conn.close()
 
     # Return the column names.
-    k_n.send(pickle.dumps(['EP', [col[i][0] for i in range(len(col))]]))
+    if not ErrorHandle.is_error(col):
+        ErrorHandle.write_socket(k_n, ['EP', [col[i][0] for i in range(len(col))]])
+
+
+def store_from_ship(sock_n, cur, temp_name):
+    """
+
+    :param sock_n:
+    :param cur:
+    :param temp_name:
+    :return:
+    """
+    # Wait for a response. Wait 1 second for other node to respond after.
+    r = ErrorHandle.read_socket(sock_n)
+    if ErrorHandle.is_error(r):
+        return r
+    time.sleep(1.0)
+
+    # Execute the insertion.
+    operation, resultant = r
+    r = ErrorHandle.sql_execute(cur, 'INSERT INTO ' + temp_name +
+                                ' VALUES (' +
+                                ''.join(['?, ' for _ in range(len(resultant) - 1)]) + '?);',
+                                tup=resultant)
+    if ErrorHandle.is_error(r):
+        return r
+
+    # Repeat. Operation code 'ES' marks the start of a message, and 'EZ' marks the end.
+    while operation != 'EZ':
+        r = ErrorHandle.read_socket(sock_n)
+        if ErrorHandle.is_error(r):
+            return r
+
+        operation, resultant = r
+        y = ErrorHandle.sql_execute(cur, 'INSERT INTO ' + temp_name +
+                                    ' VALUES (' +
+                                    ''.join(['?, ' for _ in range(len(resultant) - 1)]) + '?);',
+                                    tup=resultant)
+        if ErrorHandle.is_error(y):
+            return y
+    return 'Success'
+
+
+def copy_table(k_n, cur, node, f_s, tnames):
+    """
+
+    :param k_n:
+    :param cur:
+    :param node:
+    :param f_s:
+    :param tnames:
+    :return:
+    """
+    # Create socket to secondary node.
+    host, port, f = ClusterCFG.parse_uri(node)
+    sock_n = ErrorHandle.create_client_socket(host, port)
+    if ErrorHandle.is_error(sock_n):
+        ErrorHandle.write_socket(k_n, sock_n)
+        sock_n.close()
+        return sock_n
+
+    # Grab the SQL used to create the table.
+    ErrorHandle.write_socket(sock_n, ['E', f_s[1], 'SELECT sql '
+                                                   'FROM sqlite_master '
+                                                   'WHERE name="{}"'.format(tnames[1])])
+    r = ErrorHandle.read_socket(sock_n)
+    if ErrorHandle.is_error(r):
+        sock_n.close()
+        return r
+    operation, create_sql = r
+
+    # Execute the create table statement on the local database with a new table name.
+    new_table = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)) + 'TTTTT'
+    r = ErrorHandle.sql_execute(cur, create_sql[0].replace(tnames[1], new_table, 1) + ';')
+    if ErrorHandle.is_error(r):
+        sock_n.close()
+        return r
+
+    sock_n.close()
+    return create_sql, new_table
+
+
+def ship(k_n, r):
+    """
+
+    :param k_n:
+    :param r:
+    :return:
+    """
+    f_s, tnames, node = r[1], r[2], r[3]
+    error_handle = lambda a: ErrorHandle.write_socket(k_n, str(a))
+
+    # Connect to local database.
+    c = ErrorHandle.sql_connect(f_s[0], error_handle)
+    if ErrorHandle.is_error(c):
+        return
+    conn, cur = c
+
+    # Copy the table from our remote node to our local node.
+    r = copy_table(k_n, cur, node, f_s, tnames)
+    if ErrorHandle.is_error(r):
+        error_handle(r)
+        return
+    create_sql, new_table = r
+
+    # Create socket to secondary node.
+    host, port, f = ClusterCFG.parse_uri(node)
+    sock_n = ErrorHandle.create_client_socket(host, port)
+    if ErrorHandle.is_error(sock_n):
+        error_handle(sock_n)
+        return
+
+    # Retrieve data from secondary node.
+    ErrorHandle.write_socket(sock_n, ['E', f_s[1], 'SELECT * FROM ' + tnames[1]])
+    r = store_from_ship(sock_n, cur, new_table)
+    if ErrorHandle.is_error(r):
+        error_handle(r)
+        return
+
+    # Return the name of the table created if successful.
+    conn.commit(), conn.close()
+    ErrorHandle.write_socket(k_n, ['EB', new_table])
 
 
 def interpret(k_n, r):
@@ -176,7 +300,7 @@ def interpret(k_n, r):
 
     # If our response is not an list, return an error.
     if not hasattr(r, '__iter__'):
-        k_n.send(pickle.dumps('Input not an list.')), k_n.close()
+        ErrorHandle.write_socket(k_n, 'Error: Input not a list.'), k_n.close()
         return
 
     if r[0] == 'YS':
@@ -186,13 +310,13 @@ def interpret(k_n, r):
         # Execute a single insertion operation on a database.
         insert_single_on_db(k_n, r)
     elif r[0] == 'YY':
-        # Do not perform any action. Send a success message
-        k_n.send(pickle.dumps(['EY', 'Success']))
+        # Do not perform any action. Send a success message.
+        ErrorHandle.write_socket(k_n, ['EY', 'Success'])
     elif r[0] == 'E':
         # Execute an operation on a database.
         execute_on_db(k_n, r)
     elif r[0] == 'C':
-        # Insert ddl into the catalog table.
+        # Execute DDL on the catalog table.
         LocalCatalog.record_ddl(k_n, r)
     elif r[0] == 'K':
         # Insert partition data into the catalog table.
@@ -203,8 +327,11 @@ def interpret(k_n, r):
     elif r[0] == 'P':
         # Return the columns associated with the given table.
         return_columns(k_n, r)
+    elif r[0] == 'B':
+        # Ship a table from a remote node to here, and perform a join.
+        ship(k_n, r)
     else:
-        k_n.send(pickle.dumps('Operation code invalid.'))
+        ErrorHandle.write_socket(k_n, 'Error: Operation code invalid.')
 
 
 if __name__ == '__main__':
@@ -215,10 +342,9 @@ if __name__ == '__main__':
 
     # Create the socket.
     sock = socket.socket()
-    try:
-        sock.bind((sys.argv[1], int(sys.argv[2])))
-    except OSError as e:
-        print('Socket Error: ' + str(e))
+    r_n = ErrorHandle.attempt_operation(lambda: sock.bind((sys.argv[1], int(sys.argv[2]))),
+                                        OSError, lambda a: print('Error: ' + str(a)))
+    if ErrorHandle.is_error(r_n):
         exit(3)
 
     while True:
@@ -228,9 +354,9 @@ if __name__ == '__main__':
 
         try:
             # Retrieve the sent data. Unpickle the data. Interpret the command.
-            interpret(k, pickle.loads(k.recv(4096)))
+            interpret(k, ErrorHandle.read_socket(k))
         except EOFError as e:
-            k.send(pickle.dumps(str(e)))
+            ErrorHandle.write_socket(k, str(e))
         except ConnectionResetError as e:
             # Ignore when a connection is forcibly closed, or the socket has timed out.
             pass
--- runSQL.py
+++ runSQL.py
@@ -1,150 +1,61 @@
 # coding=utf-8
 """
-Executes a **simple** single SQL statement on a cluster of computers. This SQL statement is
-specified in 'sqlfile', and access information about each computer in the cluster is specified
-in 'clustercfg'.
+Executes various functions on a cluster of nodes. Function is determined using the given arguments.
 
-Usage: python runSQL.py [clustercfg] [sqlfile]
+| Cluster config file contains... | SQL file contains... | Function
+| tablename                       | N/A                  | Bulk load a CSV to the cluster.
+| NOT tablename                   | DDL                  | Execute a DDL across the cluster.
+| NOT tablename                   | No joins             | Execute (simple) SQL across the cluster.
+| NOT tablename                   | Joins are present    | Execute (join) SQL across the cluster.
 
+Usage: python runSQL.py [clustercfg] [sqlfile/csvfile]
+
+TODO: Finish the error codes below.
 Error: 2 - Incorrect number of arguments.
        3 - There exists an error with the clustercfg file.
        4 - There exists an error with the SQL file.
-       5 - Catalog could not be reached or the table was not found.
-       6 - Table not found in SQL file.
+
 """
 
-import ctypes
 import os
-import pickle
-import socket
+import subprocess
 import sys
-from multiprocessing import Process, Array
-from subprocess import call
 
-from catalog import RemoteCatalog
-from dissect import SQLFile, ClusterCFG
-
-
-def collect_socket(k):
-    """ Given the socket and the name of the node, grab the operation and the tuple results. This
-    is to be executed after sending out an execute command 'E'.
-
-    :param k: Open to socket to wait for a response on.
-    :return: Error message if a list is not received. Otherwise, the operation and tuple as a list.
-    """
-    response = k.recv(4096)
-    try:
-        r = pickle.loads(response) if response != b'' else 'No response received.'
-
-        if isinstance(r, str) and r == '':
-            # An empty string here means we didn't receive a response.
-            return 'Failed to receive response.'
-        elif isinstance(r, str):
-            # A string entry here means that an error occurred. Return the resultant.
-            return r
-        elif hasattr(r, '__iter__') and len(r) >= 2:
-            # The response is correctly formatted. Return the operation and the resultant.
-            operation, resultant = r
-        else:
-            # Resultant is a single-element list.
-            return 'Invalid response. Response returned is a singular element.'
-    except EOFError as e:
-        return str(e)
-
-    return operation, resultant
-
-
-def execute_sql(node_uri, n, s_n, s_nodes_n):
-    """ Given the URI of a node from the clustercfg file and the SQL to execute, send the SQL to
-    the appropriate node. Print any return messages or errors that occur.
-
-    :param node_uri: Node URI from the clustercfg file (right side of key-value pair).
-    :param n: Node number that this operation is working on.
-    :param s_n: SQL statement to execute on the node.
-    :param s_nodes_n: Successful nodes array to record to.
-    :return: None.
-    """
-    host, sock = node_uri.split(':', 1)[0], ''
-    port, f = node_uri.split(':', 1)[1].split('/', 1)
-
-    # Create our socket.
-    try:
-        sock = socket.socket()
-        sock.connect((host, int(port)))
-    except OSError as e:
-        print('Error on Node: ' + str(n) + ': ' + str(e)), sock.close()
-        return
-
-    # Pickle our command list ('E', filename, and SQL), and send our message.
-    sock.send(pickle.dumps(['E', f, s_n]))
-
-    # Wait for a response.
-    r = collect_socket(sock)
-    if isinstance(r, str):
-        print('Error on Node ' + str(n) + ': ' + r), sock.close()
-        return
-    else:
-        operation, resultant = r
-        if isinstance(resultant, str):
-            print('Node ' + str(n) + ': ' + resultant)
-        else:
-            print('Node ' + str(n) + ': [' + ''.join([str(x) + ', ' for x in resultant]) + ']')
-
-    # Repeat. Operation code 'ES' marks the start of a message, and 'EZ' marks the end.
-    while operation != 'EZ':
-        r = collect_socket(sock)
-        if isinstance(r, str):
-            print('Error on Node ' + str(n) + ': ' + r), sock.close()
-            return
-        else:
-            operation, resultant = r
-            print('Node ' + str(n) + ': [' + ''.join([str(x) + ', ' for x in resultant]) + ']')
-
-    # End is reached. The operation was successful.
-    s_nodes_n[n - 1] = n
-    sock.close()
-
+from lib.dissect import ClusterCFG, SQLFile
+from lib.error import ErrorHandle
 
 if __name__ == '__main__':
     # Ensure that we have only two arguments.
     if len(sys.argv) != 3:
-        print('Usage: python3 runSQL.py [clustercfg] [sqlfile]'), exit(2)
+        print('Usage: python3 runSQL.py [clustercfg] [sqlfile/csvfile]'), exit(2)
 
-    # Parse both the clustercfg and sqlfile. Ensure that both are properly formatted.
-    c_r, s_r = ClusterCFG.catalog_uri(sys.argv[1]), SQLFile.as_string(sys.argv[2])
-    if isinstance(c_r, str):
-        print('Error: ' + c_r), exit(3)
-    elif isinstance(s_r, str):
-        print('Error: ' + s_r), exit(4)
-    catalog_uri, s = c_r[0], s_r[0]
+    r = ClusterCFG.is_runLSCV(sys.argv[1])
+    if ErrorHandle.is_error(r):
+        print('Error: ' + r), exit(3)
 
-    # If the given SQL is a DDL, call 'runDDL' instead.
+    # The user wants to load a CSV.
+    if r:
+        print('Desired Function: Bulk CSV load')
+        exit(subprocess.call(['python' + ('3' if (os.name != 'nt') else ''), 'run/runLCSV.py',
+                              sys.argv[1], sys.argv[2]]))
+
+    # The user wants to execute some SQL, we look at the SQL file.
+    s = SQLFile.as_string(sys.argv[2])
+    if ErrorHandle.is_error(s):
+        print(s), exit(4)
+
+    # The user wants to execute a DDL.
     if SQLFile.is_ddl(s):
-        call(['python' + ('3' if (os.name != 'nt') else ''), 'runDDL.py',
-              sys.argv[1], sys.argv[2]]), exit(0)
-
-    # Determine the working table.
-    t_table = SQLFile.table(s)
-    if not t_table:
-        print('Error: Table could not be found in \'sqlfile\'.'), exit(6)
-
-    # Collect our node URIs. Do not proceed if the catalog node cannot be reached.
-    node_uris = RemoteCatalog.return_node_uris(catalog_uri, t_table)
-    if isinstance(node_uris, str):
-        print('Catalog Error: ' + node_uris), exit(5)
-
-    # Create the shared memory segment (successfully completed nodes).
-    s_nodes = Array(ctypes.c_int, len(node_uris))
-
-    # For every node in the cluster, execute the given statement and display any errors.
-    processes = []
-    for i, node in enumerate(node_uris):
-        processes.append(Process(target=execute_sql, args=(node, i + 1, s, s_nodes)))
-        processes[i].start()
-    [b.join() for b in processes]
-
-    # Display a summary: which nodes were successful and which nodes were not.
-    print('\nSummary: ')
-    for i, node in enumerate(node_uris):
-        sp = 'Node ' + str(i + 1) + '[' + node + ']: '
-        print(sp + ('Successful' if (i + 1) in s_nodes else 'Failed'))
+        print('Desired Function: Cluster DDL Execution')
+        exit(subprocess.call(['python' + ('3' if (os.name != 'nt') else ''), 'run/runDDL.py',
+                              sys.argv[1], sys.argv[2]]))
+    elif SQLFile.is_join(s):
+        # The user wants to execute SQL involving a join.
+        print('Desired Function: Cluster SQL Selection with Join')
+        exit(subprocess.call(['python' + ('3' if (os.name != 'nt') else ''), 'run/runDJSQL.py',
+                              sys.argv[1], sys.argv[2]]))
+    else:
+        # The user wants to execute SQL without joins.
+        print('Desired Function: Cluster SQL Execution')
+        exit(subprocess.call(['python' + ('3' if (os.name != 'nt') else ''), 'run/runSSQL.py',
+                              sys.argv[1], sys.argv[2]]))
